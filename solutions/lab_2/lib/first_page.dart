bedd85e491811b1181f0f5f7245f3df404dc19d1
Robbin <robbinl@telusplanet.net>
2019-10-02 10:55:26 -0600

    a

--- solutions/lab_2/lib/first_page.dart
+++ solutions/lab_2/lib/first_page.dart
@@ -7,25 +7,34 @@ class MyFirstPage extends StatefulWidget {
 
 class _MyFirstPageState extends State<MyFirstPage> {
   bool _enabled = false;
-  bool _start = false;
-  var _onPressed;
+  bool _reset = false;
   int _timesClicked = 0;
-  String _msg = 'Button Disabled';
+  String _msg1 = 'Disabled';
+  String _msg2 = 'Disabled';
 
   @override
   Widget build(BuildContext context) {
-    Object onPressed() {
+    Object onPressed1() {
       if (_enabled) {
         return () {
           setState(() {
-            if (_start) {
-              _timesClicked++;
-              _msg = 'Clicked $_timesClicked yes man';
-            } else {
-              _timesClicked = 0;
-              _msg = 'Click Here';
-            }
+            _timesClicked++;
+            _msg1 = 'Clicked $_timesClicked';
             print('clicked $_timesClicked');
+          });
+        };
+      } else {
+        return null;
+      }
+    }
+
+    Object onPressed2() {
+      if (_enabled) {
+        return () {
+          setState(() {
+            _timesClicked = 0;
+            _msg1 = 'Click Me';
+            print('Reset Pressed');
           });
         };
       } else {
@@ -40,62 +49,63 @@ class _MyFirstPageState extends State<MyFirstPage> {
       body: Container(
         child: Column(
           children: <Widget>[
-            Container(
-              child: MaterialButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
-                elevation: 8,
-                textColor: Colors.red.shade100,
-                color: Colors.redAccent,
-                highlightColor: Colors.blue,
-                splashColor: Colors.green.shade300,
-                padding: EdgeInsets.all(20.0),
-                onPressed: onPressed(),
-                child: Text(_msg),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('enable functionality'),
+                Switch(
+                    value: _enabled,
+                    onChanged: (bool value) {
+                      setState(() {
+                        _enabled = value;
+                        if (_enabled) {
+                          _msg1 = 'Click Me';
+                          _msg2 = 'Reset';
+                          print('_enabled is true');
+                        } else {
+                          _msg1 = 'Disabled';
+                          _msg2 = 'Disabled';
+                          print('_enabled is false');
+                        }
+                      });
+                    }),
+              ],
             ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Text('disable/enable button'),
-                  Switch(
-                      value: _enabled,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _enabled = value;
-                          if (_enabled) {
-                            _msg = 'Clicked $_timesClicked';
-                            print('_enabled is true');
-                          } else {
-                            _msg = 'Button Disabled';
-                            print('_enabled is false');
-                          }
-                        });
-                      }),
-                ],
-              ),
-            ),
-            Container(
-              child: Row(
-                children: <Widget>[
-                  Text('reset/start button'),
-                  Switch(
-                      value: _start,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _start = value;
-                          if (_start) {
-                            _msg = 'Clicked $_timesClicked here man';
-                            print('_start is true');
-                          } else {
-                            _timesClicked = 0;
-                            _msg = 'Click Here';
-                            print('_start is false');
-                          }
-                        });
-                      }),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    elevation: 8,
+                    textColor: Colors.red.shade100,
+                    color: Colors.redAccent,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.green.shade300,
+                    padding: EdgeInsets.all(20.0),
+                    onPressed: onPressed1(),
+                    child: Text(_msg1),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 8,
+                    textColor: Colors.red.shade100,
+                    color: Colors.blue,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.green.shade300,
+                    padding: EdgeInsets.all(20.0),
+                    onPressed: onPressed2(),
+                    child: Text(_msg2),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
