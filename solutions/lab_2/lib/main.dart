ad07ccdeb3dff91f2cd6a59f22368d781c156bbb
Robbin <robbinl@telusplanet.net>
2019-10-02 11:10:48 -0600

    a

--- solutions/lab_2/lib/main.dart
+++ solutions/lab_2/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'first_page.dart';
+//import 'second_page.dart';
 
 void main() {
   runApp(MyApp());
--- solutions/lab_2/lib/second_page.dart
+++ solutions/lab_2/lib/second_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
-class MyFirstPage extends StatefulWidget {
+class MySecondPage extends StatefulWidget {
   @override
-  _MyFirstPageState createState() => _MyFirstPageState();
+  _MySecondPageState createState() => _MySecondPageState();
 }
 
-class _MyFirstPageState extends State<MyFirstPage> {
+class _MySecondPageState extends State<MySecondPage> {
   bool _enabled = false;
+  int _timesClicked = 0;
   String _msg1 = '';
+  String _msg2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -15,8 +17,23 @@ class _MyFirstPageState extends State<MyFirstPage> {
       if (_enabled) {
         return () {
           setState(() {
-            _msg1 = 'Enabled';
-            print('Button Pressed');
+            _timesClicked++;
+            _msg1 = 'Clicked $_timesClicked';
+            print('clicked $_timesClicked');
+          });
+        };
+      } else {
+        return null;
+      }
+    }
+
+    Object onPressed2() {
+      if (_enabled) {
+        return () {
+          setState(() {
+            _timesClicked = 0;
+            _msg1 = 'Click Me';
+            print('Reset Pressed');
           });
         };
       } else {
@@ -41,12 +58,12 @@ class _MyFirstPageState extends State<MyFirstPage> {
                       setState(() {
                         _enabled = value;
                         if (_enabled) {
-                          _msg1 = 'Enabled';
-
+                          _msg1 = 'Click Me';
+                          _msg2 = 'Reset';
                           print('_enabled is true');
                         } else {
                           _msg1 = '';
-
+                          _msg2 = '';
                           print('_enabled is false');
                         }
                       });
@@ -69,6 +86,22 @@ class _MyFirstPageState extends State<MyFirstPage> {
                     padding: EdgeInsets.all(20.0),
                     onPressed: onPressed1(),
                     child: Text(_msg1),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 8,
+                    textColor: Colors.red.shade100,
+                    color: Colors.blue,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.green.shade300,
+                    padding: EdgeInsets.all(20.0),
+                    onPressed: onPressed2(),
+                    child: Text(_msg2),
                   ),
                 ),
               ],
