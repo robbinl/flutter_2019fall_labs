31e0de167db4b4cecb183c4b774ed388298b652f
Robbin <robbinl@telusplanet.net>
2019-10-17 14:50:20 -0600

    a

---
+++ lab_3_begin/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:coffee_store_app/widget/StoreHomePage.dart';
+import 'package:coffee_store_app/widget/tabbed_page_demo_1.dart';
+import 'package:coffee_store_app/widget/tabbed_page_demo_2.dart';
+import 'package:coffee_store_app/widget/tabbed_page_demo_3.dart';
+import 'package:coffee_store_app/widget/scoped_model_demo_1.dart';
+
+//void main() => runApp(MyTabbedApp1());
+//void main() => runApp(MyTabbedApp2());
+//void main() => runApp(MyTabbedApp3());
+//void main() => runApp(MyScopeModelApp1(model: CounterModel()));
+void main() => runApp(MyAppLab3());
