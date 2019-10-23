bb30fe7ebbc0555047f694310b77a84aed241490
Robbin <robbinl@telusplanet.net>
2019-10-23 13:19:24 -0600

    a

--- lab_3_begin/lib/main.dart
+++ lab_3_begin/lib/main.dart
@@ -8,5 +8,5 @@ import 'package:coffee_store_app/widget/scoped_model_demo_1.dart';
 //void main() => runApp(MyTabbedApp1());
 //void main() => runApp(MyTabbedApp2());
 //void main() => runApp(MyTabbedApp3());
-//void main() => runApp(MyScopeModelApp1(model: CounterModel()));
-void main() => runApp(MyAppLab3());
+void main() => runApp(MyScopeModelApp1(model: CounterModel()));
+//void main() => runApp(MyAppLab3());
