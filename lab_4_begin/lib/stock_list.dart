86f459421894f8118d08a5e6bd302b1b35701677
Robbin <robbinl@telusplanet.net>
2019-11-07 12:56:53 -0700

    a

---
+++ lab_4_begin/lib/stock_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'stock.dart';
+
+class StockList extends StatefulWidget {
+  StockList({Key key, this.stocks}) : super(key: key);
+
+  final List<Stock> stocks;
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _StockListState();
+  }
+}
+
+class _StockListState extends State<StockList> {
+  @override
+  Widget build(BuildContext context) {
+    return _buildStockList(context, widget.stocks);
+  }
+
+  ListView _buildStockList(context, List<Stock> stocks) {
+    return new ListView.builder(
+      itemCount: stocks.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          //TODO:  use title, subtitle, and trailing to display the stock symbol
+          // companyName and latestPrice
+          title: Text('Symbol: ${stocks[index].symbol}'),
+        );
+      },
+    );
+  }
+}
