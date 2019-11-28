a57aa6f272545ccba25ee7f7795250624eed8f44
Robbin <robbinl@telusplanet.net>
2019-11-28 11:33:32 -0700

    a

--- lab_3_begin/ios/Flutter/flutter_export_environment.sh
+++ lab_3_begin/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\robbinl\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\robbinl\Desktop\temp\flutter-dev\lab_3_begin"
+export "FLUTTER_APPLICATION_PATH=C:\Users\robbinl\Desktop\flutter\flutter_2019fall_labs-1\lab_3_begin"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
--- lab_4_begin/ios/Flutter/flutter_export_environment.sh
+++ lab_4_begin/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\robbinl\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\robbinl\Desktop\flutter\flutter-dev\lab_4_solution"
+export "FLUTTER_APPLICATION_PATH=C:\Users\robbinl\Desktop\flutter\flutter_2019fall_labs-1\lab_4_begin"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
